8a3bf643e6b9c1aad0e472dae688e37a35fd512c
Iago Vieira Carvalho <iagovelox@gmail.com>
2025-03-25 14:05:44 -0300

    Revert "aa"
    This reverts commit 7cd86f67a1348a697c3ee2a5772b89cddf37b5e6.

--- React-Teste/src/app/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import '../styles/global.css'
-import { Slot } from 'expo-router'
-
-export function Layout() {
-    <Slot />
-}
--- React-Teste/src/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { View } from 'react-native';
-import { Header } from '../components/header';
-export default function App() {
-  return (
-    <Header />
-  );
-}
