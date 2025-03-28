cc761b12cae70c47fb5e10296f49d55a466c0f28
Iago Vieira Carvalho <iagovelox@gmail.com>
2025-03-28 20:54:25 -0300

    atualização hovers

--- Proj-ReactTelaLoginTCC/TelaLoginTCC/src/components/screens/servicePage/content.tsx
+++ Proj-ReactTelaLoginTCC/TelaLoginTCC/src/components/screens/servicePage/content.tsx
@@ -1,9 +1,11 @@
 
 import { Pressable, View, Text, TextInput, Dimensions, } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 
 export function ContentPage() {
+    const [hover, setHover] = useState(false);
     const [width, height] = [Dimensions.get('window').width, Dimensions.get('window').height];
 
     return (
@@ -14,17 +16,17 @@ export function ContentPage() {
             gap: width <= 800 ? 10 : 15,
             borderRadius: 10,
         }} >
-            <Pressable className='shadow-md w-[180px] min-h-[100px] rounded-lg p-[20px] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
-                <Text className='font-bold text-[12pt]'>Criar </Text>
+            <Pressable className='shadow-md w-[180px] active:scale-[1.05] min-h-[100px] rounded-lg p-[20px] active:bg-green-300 items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
+                <Text className='font-bold text-[12pt] ' selectable={false}>Criar </Text>
                 <Ionicons name='create' size={26} />
             </Pressable>
 
-            <Pressable className='shadow-md w-[180px] min-h-[100px] rounded-lg p-[20px] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
-                <Text className='font-bold text-[12pt]'>Deletar </Text>
+            <Pressable className='shadow-md w-[180px] min-h-[100px] active:scale-[1.05] rounded-lg p-[20px] active:bg-red-500 items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
+                <Text className='font-bold text-[12pt]' selectable={false}>Deletar </Text>
                 <Ionicons name='trash-outline' size={26} />
             </Pressable>
-            <Pressable className='shadow-md w-[180px] min-h-[100px] rounded-lg p-[20px] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
-                <Text className='font-bold text-[12pt]'>Configurar </Text>
+            <Pressable className='shadow-md w-[180px] min-h-[100px] rounded-lg p-[20px] active:bg-blue-800 active:scale-[1.05] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
+                <Text className='font-bold text-[12pt]' selectable={false}>Configurar </Text>
                 <Ionicons name='construct-outline' size={26} />
             </Pressable>
         </View>
