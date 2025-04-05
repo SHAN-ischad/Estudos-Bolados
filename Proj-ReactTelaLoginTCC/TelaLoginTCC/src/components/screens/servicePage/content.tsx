93f455aeac261a924e5b48496d39653baad0b964
Iago Vieira Carvalho <iagovelox@gmail.com>
2025-04-05 13:25:47 -0300

    atualização
    colocando fontes em meu projeto

--- Proj-ReactTelaLoginTCC/TelaLoginTCC/src/components/screens/servicePage/content.tsx
+++ Proj-ReactTelaLoginTCC/TelaLoginTCC/src/components/screens/servicePage/content.tsx
@@ -1,32 +1,40 @@
 
 import { Pressable, View, Text, TextInput, Dimensions, } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { useState } from 'react';
-
+import { useFonts, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { styled } from '../../../style/style';
 
 export function ContentPage() {
-    const [hover, setHover] = useState(false);
+    const [fontsLoaded] = useFonts({
+        Roboto_700Bold,
+    });
+
+    if (!fontsLoaded) {
+        return null
+    }
     const [width, height] = [Dimensions.get('window').width, Dimensions.get('window').height];
 
     return (
-        <View className='w-full flex-row gap-[4%] items-center justify-center' style={{
+        <View className='w-full flex-row gap-[4%] items-center justify-center te' style={{
             flexDirection: width <= 800 ? 'column' : 'row',
             justifyContent: width <= 800 ? 'center' : 'center',
             alignItems: width <= 800 ? 'center' : 'center',
             gap: width <= 800 ? 10 : 15,
             borderRadius: 10,
+
         }} >
-            <Pressable className='shadow-md w-[180px] active:scale-[1.05] min-h-[100px] rounded-lg p-[20px]  items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
-                <Text className='font-bold text-[12pt] ' selectable={false}>Criar </Text>
+            <Pressable className='shadow-md w-[180px] min-h-[100px] 
+            rounded-lg p-[20px] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
+                <Text style={styled.fonts} className='font-bold text-[12pt]'>Criar </Text>
                 <Ionicons name='create' size={26} />
             </Pressable>
 
-            <Pressable className='shadow-md w-[180px] min-h-[100px] active:scale-[1.05] rounded-lg p-[20px]  items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
-                <Text className='font-bold text-[12pt]' selectable={false}>Deletar </Text>
+            <Pressable className='shadow-md w-[180px] min-h-[100px] rounded-lg p-[20px] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
+                <Text style={styled.fonts} className='font-bold text-[12pt]'>Deletar </Text>
                 <Ionicons name='trash-outline' size={26} />
             </Pressable>
-            <Pressable className='shadow-md w-[180px] min-h-[100px] rounded-lg p-[20px]  active:scale-[1.05] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
-                <Text className='font-bold text-[12pt]' selectable={false}>Configurar </Text>
+            <Pressable className='shadow-md w-[180px] min-h-[100px] rounded-lg p-[20px] items-center justify-center hover:bg-blue-200 hover:mt-[0.5%] duration-300 flex-row '>
+                <Text style={styled.fonts} className='font-bold text-[12pt]'>Configurar </Text>
                 <Ionicons name='construct-outline' size={26} />
             </Pressable>
         </View>
---
+++ Proj-ReactTelaLoginTCC/TelaLoginTCC/src/style/style.ts
@@ -0,0 +1,13 @@
+
+import { StyleSheet } from "react-native";
+
+export const styled = StyleSheet.create({
+    fonts: {
+
+        fontFamily: 'Roboto_700Bold',
+        fontSize: 20,
+        color: '#000',
+
+
+    }
+})
