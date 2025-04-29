e1ab96f474fa1bad2a4fc1f4cd6d022af275e8e1
Iago Vieira Carvalho <iagovelox@gmail.com>
2025-04-29 17:08:37 -0300

    atualização: Melhorias
    algumas melhorias

---
+++ Proj-TreinoProvaReact/TreinoProva/src/app/Drawer/_layout.tsx
@@ -0,0 +1,18 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function Layout() {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer >
+                <Drawer.Screen
+                    name='index'
+                    options={{
+                        headerShown: false,
+                        drawerIcon: () => <Ionicons name='home' size={22} color={'black'} />,
+                    }} />
+            </Drawer>
+        </GestureHandlerRootView>
+    );
+}
---
+++ Proj-TreinoProvaReact/TreinoProva/src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import '../../global.css'
+export default function LayoutHome() {
+    return (
+        <Stack
+            screenOptions={{
+
+                headerShown: false,
+            }}>
+            {/* Optionally configure static options outside the route.*/}
+            <Stack.Screen name="index" options={{ animation: 'flip' }} />
+            <Stack.Screen name="Drawer" options={{ animation: 'flip' }} />
+        </Stack>
+    );
+}
