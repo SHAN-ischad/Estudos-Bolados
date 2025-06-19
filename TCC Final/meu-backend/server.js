6ea5e90bfc12624837666ff73d10a90fbb04b8d5
SHAN-ischad <iagovelox@gmail.com>
2025-06-18 21:21:00 -0300

    Atualização: Back-End completo
    com a conexão com o mongo Funcionando, este back-end está pronto para se conectar/comunicar com o front

--- TCC Final/meu-backend/server.js
+++ TCC Final/meu-backend/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const cors = require('cors');
+
 const exemploRoutes = require('./routes/exemploRoutes');
 const oficinaRoutes = require('./routes/oficinaRoutes');
 const clienteRoutes = require('./routes/clientesRoutes');
@@ -9,33 +12,35 @@ const servicoRoutes = require('./routes/servicoRoutes');
 const pagamentoRoutes = require('./routes/pagamentoRoutes');
 const agendamentoRoutes = require('./routes/agendamentoRoutes');
 const chatRoutes = require('./routes/chatRoutes');
-const notificacaoRoutes = require('./routes/notificacaoRoutes');
 
-dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Configura o CORS para aceitar requisições de qualquer origem
+app.use(cors());
+
+// Middleware para interpretar JSON
 app.use(express.json());
 
 // Rotas
-app.use('/api/exemplos', exemploRoutes);
+app.use('/api/exemplo', exemploRoutes);
 app.use('/api/oficinas', oficinaRoutes);
 app.use('/api/clientes', clienteRoutes);
 app.use('/api/carros', carroRoutes);
 app.use('/api/servicos', servicoRoutes);
 app.use('/api/pagamentos', pagamentoRoutes);
 app.use('/api/agendamentos', agendamentoRoutes);
-app.use('/api/chats', chatRoutes);
-app.use('/api/notificacoes', notificacaoRoutes);
+app.use('/api/chat', chatRoutes);
 
 // Conexão com o MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('Conectado ao MongoDB'))
   .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
-// Inicia o servidor
+// Inicializa o servidor
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`😎 Servidor rodando na porta ${PORT}`);
+  console.log(`🟢 Servidor rodando na porta ${PORT}`);
 });
