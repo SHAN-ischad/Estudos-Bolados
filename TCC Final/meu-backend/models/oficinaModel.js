e8f214eb2a3eb3ec1fa68f7793fd13b0b6339801
Iago Vieira Carvalho <iagovelox@gmail.com>
2025-06-30 17:25:53 -0300

    Nada demais

--- TCC Final/meu-backend/models/oficinaModel.js
+++ TCC Final/meu-backend/models/oficinaModel.js
@@ -1,51 +1,52 @@
 const mongoose = require('mongoose');
 
 const OficinaSchema = new mongoose.Schema({
-  nome: { 
-    type: String, 
-    required: true, 
-    minlength: 3, 
-    maxlength: 50, 
+  nome: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
     match: /^[A-Za-zÀ-ÿ\s]+$/ // Apenas letras e espaços
   },
-  endereco: { 
-    type: String, 
-    required: true, 
-    minlength: 5, 
-    maxlength: 100 
+  endereco: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 100
   },
-  telefone: { 
-    type: String, 
-    required: true, 
+  telefone: {
+    type: String,
+    required: true,
     match: /^[0-9]{10,11}$/ // Apenas números, 10 ou 11 dígitos
   },
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true, 
+  email: {
+    type: String,
+    required: true,
+    unique: true,
     match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ // Regex para e-mail
   },
-  cnpj: { 
-    type: String, 
-    required: true, 
-    unique: true, 
+  cnpj: {
+    type: String,
+    required: true,
+    unique: true,
     match: /^\d{14}$/ // 14 dígitos numéricos
   },
-  proprietario: { 
-    type: String, 
-    required: true, 
-    minlength: 3, 
-    maxlength: 50, 
-    match: /^[A-Za-zÀ-ÿ\s]+$/ 
+  proprietario: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+    match: /^[A-Za-zÀ-ÿ\s]+$/
   },
-  senha: { 
-    type: String, 
-    required: true, 
-    minlength: 6 
+  senha: {
+    type: String,
+    required: true,
+    minlength: 6
   },
   dataCadastro: { type: Date, default: Date.now },
   servicosOferecidos: [{ type: String }],
-  avaliacao: { type: Number, default: 0, min: 0, max: 5 }
+  avaliacao: { type: Number, default: 0, min: 0, max: 5 },
+
 });
 
 module.exports = mongoose.model('Oficina', OficinaSchema);
