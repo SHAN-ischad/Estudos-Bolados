edc367234015ffa86e8db458e163493e880c4cd2
Iago Vieira Carvalho <iagovelox@gmail.com>
2026-01-09 22:29:30 -0300

    Create globalStyles.ts
    arquivo de estilização com informações que uso em meus projetos

---
+++ globalStyles.ts
@@ -0,0 +1,71 @@
+
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    // Fonts
+    JetBrainsFont: { fontFamily: 'JetBrainsMono-Bold' },
+    googleScansCode: { fontFamily: 'GoogleSansCode-Regular' },
+
+    // ------------------------------------------------------
+
+    // Size Fonts
+    megaFont: { fontSize: 35 },
+    extraLargeFont: { fontSize: 24 },
+    largeFont: { fontSize: 22 },
+    highFont: { fontSize: 20 },
+    mediumFont: { fontSize: 18 },
+    lowFont: { fontSize: 16 },
+    smallFont: { fontSize: 14 },
+    verySmallFont: { fontSize: 12 },
+
+
+    // ------------------------------------------------------
+
+    // Colors
+
+    // cores para blocos/colors for blocks/sections
+    whiteColor: { backgroundColor: "#FFFFFF" },
+    redColor: { backgroundColor: "#CF4137" },
+    orangeColor: { backgroundColor: "#F7760C" },
+    purpleColor: { backgroundColor: "#8333FF" },
+    purpleColorTransparency: { backgroundColor: "rgba(131, 51, 255, 0.7)" },
+    grayColor: { backgroundColor: "#3A3A3A" },
+    redColorError: { backgroundColor: '#F50404' },
+    blackColorLow: { backgroundColor: "#3F3F3E" },
+    // ---------------------------------------------------
+
+    // Cores de fundos/Backgrounds colors
+    blackColor: { backgroundColor: "#33302D" },
+    // ---------------------------------------------------
+
+    // Cores para as fontes/colors for fonts
+    bronwFonts: { color: "#4F413F" },
+    whiteFonts: { color: "#FFFFFF" },
+    redFonts: { color: "#F44516" },
+    blueFonts: { color: "#0000FF" },
+    purpleFonts: { color: "#9900FF" },
+    // ---------------------------------------------------
+
+    // cores para botões/colors for buttons
+    purpleButton: { backgroundColor: "#7860FC" },
+    whiteButton: { backgroundColor: "#B5AFAE" },
+
+    // ---------------------------------------------------
+
+    // Image Sizes
+
+    smallImageLogo: { width: 150, height: 150 },
+    extraSmallImageLogo: { width: 90, height: 90 },
+    verySmallImageLogo: { width: 45, height: 45 },
+
+    // ---------------------------------------------------
+
+    // cores para bordas/colors for borders
+    grayBorder: { borderColor: "#505050" },
+    blackBorder: { borderColor: "#33302D" },
+    whiteBorder: { borderColor: "#FFFFFF" },
+    greenBorder: { borderColor: "#34D399" },
+
+    // ---------------------------------------------------
+
+})
